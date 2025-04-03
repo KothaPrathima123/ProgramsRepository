4a55f9f3bf0a13f0ea6e9b554e5df9dce0161218
Niharika <Niharika@DESKTOP-GHDTDAD>
2025-04-03 17:47:25 +0530

    commit

--- IOCProj7/src/main/java/com/pk/test/ClientTest.java
+++ IOCProj7/src/main/java/com/pk/test/ClientTest.java
@@ -6,11 +6,8 @@ import com.pk.beans.Bike;
 
 public class ClientTest {
 	public static void main(String[] args) {
-		System.out.println("ClientTest.main()");
 		ClassPathXmlApplicationContext ctx=new ClassPathXmlApplicationContext("com/pk/cfgs/applicationContext.xml");
-		System.out.println("ClientTest.main()");
 		Bike b=ctx.getBean("basepulsor",Bike.class);
-		System.out.println("ClientTest.main()");
 		Bike b1=ctx.getBean("pulsor1",Bike.class);
 		Bike b2=ctx.getBean("pulsor2",Bike.class);
 		System.out.println("Parent Bike: "+b+ " Bike1: "+b1+" Bike2: "+b2);
