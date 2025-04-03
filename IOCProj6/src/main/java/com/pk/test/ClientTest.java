0dceb0a764ace1deed5c50ca3c304e8d242d864d
Niharika <Niharika@DESKTOP-GHDTDAD>
2025-04-03 16:37:01 +0530

    commit1

---
+++ IOCProj6/src/main/java/com/pk/test/ClientTest.java
@@ -0,0 +1,16 @@
+package com.pk.test;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.pk.beans.Bike;
+
+public class ClientTest {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext ctx=new ClassPathXmlApplicationContext("com/pk/cfgs/applicationContext.xml");
+		Bike b1=ctx.getBean("pulsor1",Bike.class);
+		Bike b2=ctx.getBean("pulsor2",Bike.class);
+		System.out.println("Bike1: "+b1+" Bike2: "+b2);
+	}
+	
+
+}
