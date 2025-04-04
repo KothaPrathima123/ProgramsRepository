503ac5eb44bef7a68cdf558f8a9ae73bf902f940
Niharika <Niharika@192.168.0.4>
2025-04-04 14:28:41 +0530

    commit

---
+++ IOCProj18-FactoryBean/src/main/java/com/pk/test/ClientApp.java
@@ -0,0 +1,16 @@
+package com.pk.test;
+
+import java.applet.AppletContext;
+
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+import com.pk.beans.ScheduleReminder;
+
+public class ClientApp {
+	public static void main(String[] args) {
+		FileSystemXmlApplicationContext ctx=new FileSystemXmlApplicationContext("src/main/java/com/pk/cfgs/applicationContext.xml");
+		ScheduleReminder reminder=ctx.getBean("reminder",ScheduleReminder.class);
+		System.out.println(reminder);
+		
+	}
+}
