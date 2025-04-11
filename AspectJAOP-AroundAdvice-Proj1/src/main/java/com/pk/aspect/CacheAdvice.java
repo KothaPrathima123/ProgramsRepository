a0d9d4631ea815cc24eb36cc7deb9314f277db8b
Niharika <Niharika@DESKTOP-GHDTDAD>
2025-04-11 15:29:46 +0530

    commit

---
+++ AspectJAOP-AroundAdvice-Proj1/src/main/java/com/pk/aspect/CacheAdvice.java
@@ -0,0 +1,24 @@
+package com.pk.aspect;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+public class CacheAdvice {
+HashMap<String,Object> cache=new HashMap<String, Object>();
+public Object caching(ProceedingJoinPoint pjp) throws Throwable{
+	String key=pjp.getSignature()+Arrays.toString(pjp.getArgs());
+	if(cache.containsKey(key)) {
+		Object retVal=pjp.proceed();
+		cache.put(key, retVal);
+		System.out.println("From Target Method");
+		return retVal;
+	}
+	else {
+		System.out.println("From cache");
+		return cache.get(key);
+	}
+}
+
+
+}
