5abd5ee73b40ee166b757aa0e7a32c681534e4d0
DESKTOP-GHDTDAD\Niharika <kotha.prathima963@gmail>
2025-05-12 10:41:23 +0530

    Jms commit

---
+++ BootMSProj02-SubscriberApp-PUB-SUB/src/main/java/com/pk/BootMsProj01ReceiverAppApplication.java
@@ -0,0 +1,13 @@
+package com.pk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMsProj01ReceiverAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMsProj01ReceiverAppApplication.class, args);
+	}
+
+}
